07f7ec7603220b26309256071550f5713957e07e
Magi Karatasevska <magi.karatasevska@gmail.com>
2023-07-20 22:17:01 +0200

    last try

---
+++ server.js
@@ -0,0 +1,106 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(express.json())
+
+app.use(morgan('tiny'))
+
+const PORT = 3001
+
+morgan.token('object', function (req, res) {
+    return `${JSON.stringify(req.body)}`
+})
+app.use(morgan(':object'))
+
+// this prints it in the terminal duplo
+//app.use(morgan(':method :url :status :res[content-length] - :response-time ms :object'))
+
+let persons = [
+    { 
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
+      },
+      { 
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
+      },
+      { 
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
+      },
+      { 
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
+      }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+
+})
+
+app.get('/info', (req, res) => {
+    const currentDate = new Date()
+    res.send(`<h2>Phonebook has info for ${persons.length} people</h2> <h2>${currentDate}</h2>`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const entry = persons.find(entry => entry.id == id)
+
+    if(entry){
+        res.json(entry)
+    }else {
+        res.status(404).end()
+    }
+
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(entry => entry.id != id)
+
+    res.status(204).end()
+})
+
+const generateId = () => {
+    const maxID = persons.length > 0
+    ? Math.max( ...persons.map(n => n.id))
+    : 0
+    return maxID +1
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if(!body.name || !body.number){
+        return res.status(400).json({ error: 'The name or number is missing'})
+    }
+
+    if(persons.some(entry => entry.name === body.name)){
+        return res.status(409).json({ error: 'name must be unique' })
+    }
+
+
+
+    let entry = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons.push(entry)
+
+    res.json(entry)
+
+})
+
+app.listen(PORT, () => {
+    console.log(`Server active on ${PORT}`)
+})
